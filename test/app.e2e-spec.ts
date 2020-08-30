2edb4a07fe0719efe4f20d802ac4e1b806f07854
Eduardo Dib <eduardodib11@gmail.com>
2020-08-30 13:34:03 -0300

    This fail now

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -18,7 +18,7 @@ describe('AppController (e2e)', () => {
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
-      .expect(300)
-      .expect('Hello World Fail!');
+      .expect(200)
+      .expect('Hello World!');
   });
 });
